c09485849e04c7eab75d73564f1c318ce6116bf2
Ksenia001 <kseniabelko01@gmail.com>
2025-04-20 12:14:48 +0300

    лаб 4

--- src/main/java/com/example/myspringproject/dto/create/CategoryCreateDto.java
+++ src/main/java/com/example/myspringproject/dto/create/CategoryCreateDto.java
@@ -1,7 +1,6 @@
 package com.example.myspringproject.dto.create;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.util.List;
